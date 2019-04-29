e34771fe914d8be1320e9beab41e4f563dcf86e7
Liron Ben Dror <48080144+lbendror@users.noreply.github.com>
2019-04-29 06:12:31 +0300

    Update ResultsSidebarOnAmazon.java

--- JB2019AmazonProject/src/main/java/tests/ResultsSidebarOnAmazon.java
+++ JB2019AmazonProject/src/main/java/tests/ResultsSidebarOnAmazon.java
@@ -25,7 +25,7 @@ public class ResultsSidebarOnAmazon extends AbstractTest{
 		browseToUrl("http://amazon.com");
 		AmazonLandingPage amazonLandingPage = new AmazonLandingPage(driver);
 		report.endLevel();
-		
+
 		//Step 2 - Write a random search in the top search bar and click the search button.
 		//Show results for the searching written.
 		report.startLevel("Step 2 - Write a random search in the top search bar and click the search button.");
@@ -33,16 +33,16 @@ public class ResultsSidebarOnAmazon extends AbstractTest{
 		amazonLandingPage.writeToSearchbox(randomSearchTerm);
 		AmazonSearchResultsPage amazonSearchResultsPage = amazonLandingPage.clickOnGoButton();
 		report.endLevel();
-		
+
 		//Step 3 - On the LEFT results, Refine by Condition New.	
 		//The show results, result by Condition.
 		report.startLevel("Step 3 - On the LEFT results, Refine by Condition New.");
 		ResultsSidebarOnAmazonPage.RefineByCondition();
 		report.endLevel();
 
-		//Step 4 - On the results by Condition Click on ‚ÄòCell Phones‚Äô Department.	
+		//Step 4 - On the results by Condition Click on ‘Cell Phones’ Department.	
 		//Return to results for the searching chosen.
-		report.startLevel("Step 4 - On the results by Condition Click on ‚ÄòCell Phones‚Äô Department.");
+		report.startLevel("Step 4 - On the results by Condition Click on ‘Cell Phones’ Department.");
 		ResultsSidebarOnAmazonPage.RefineByDepartment();
 		report.endLevel();
 
@@ -57,47 +57,47 @@ public class ResultsSidebarOnAmazon extends AbstractTest{
 		report.startLevel("Step 6 - On the results by Condition Click on Refine by Brand.");
 		ResultsSidebarOnAmazonPage.Brand();
 		report.endLevel();
-		
-    	//Step 7 - Open the button name Sort by, and choose to Sort by: ‚ÄúNewest Arrivals‚Äù.
-		//The Departments chosen is now Sort by: ‚ÄúNewest Arrivals‚Äù.
-		report.startLevel("Step 7 - Open the button name Sort by, and choose to Sort by: ‚ÄúNewest Arrivals‚Äù");
+
+		//Step 7 - Open the button name Sort by, and choose to Sort by: “Newest Arrivals”.
+		//The Departments chosen is now Sort by: “Newest Arrivals”.
+		report.startLevel("Step 7 - Open the button name Sort by, and choose to Sort by: “Newest Arrivals”");
 		driver.findElements(By.cssSelector("span.a-button-inner")).get(0).click();
 		List<WebElement> SortByButtonNewestArrivals = driver.findElements(By.cssSelector("div.a-popover-inner ul li"));
 		SortByButtonNewestArrivals.get(4).click();
 		report.endLevel();
-		
-		//Step 8 - Open the button name Sort by, and choose to Sort by: ‚ÄúAvg. Customer Review‚Äù.	
-		//The Departments chosen is now Sort by: ‚ÄúAvg. Customer Review‚Äù.
-		report.startLevel("Step 8 - Open the button name Sort by, and choose to Sort by: ‚ÄúAvg. Customer Review‚Äù");
+
+		//Step 8 - Open the button name Sort by, and choose to Sort by: “Avg. Customer Review”.	
+		//The Departments chosen is now Sort by: “Avg. Customer Review”.
+		report.startLevel("Step 8 - Open the button name Sort by, and choose to Sort by: “Avg. Customer Review”");
 		driver.findElements(By.cssSelector("span.a-button-inner")).get(0).click();
 		List<WebElement> SortByButtonAvgCustomerReviewButton = driver.findElements(By.cssSelector("div.a-popover-inner ul li"));
 		SortByButtonAvgCustomerReviewButton.get(3).click();
 		report.endLevel();
-		
+
 		//Step 9 - Open the button name Sort by, and choose to Sort by: "Price: High to Low".	
-		//The Departments chosen is now Sort by: ‚ÄúHigh to Low‚Äù.
+		//The Departments chosen is now Sort by: “High to Low”.
 		report.startLevel("Step 9 - Open the button name Sort by, and choose to Sort by: Price: High to Low");
 		driver.findElements(By.cssSelector("span.a-button-inner")).get(0).click();
 		List<WebElement> SortByButtonPriceHighToLow = driver.findElements(By.cssSelector("div.a-popover-inner ul li"));
 		SortByButtonPriceHighToLow.get(2).click();
 		report.endLevel();
-		
-		//Step 10 - Open the button name Sort by, and choose to Sort by: ‚ÄúPrice: Low to High‚Äù.	
-		//The Departments chosen is now Sort by: ‚ÄúLow to High‚Äù.
+
+		//Step 10 - Open the button name Sort by, and choose to Sort by: “Price: Low to High”.	
+		//The Departments chosen is now Sort by: “Low to High”.
 		report.startLevel("Step 10 - Open the button name Sort by, and choose to Sort by: Price: Low to High");
 		driver.findElements(By.cssSelector("span.a-button-inner")).get(0).click();
 		List<WebElement> SortByButtonPriceLowToHigh = driver.findElements(By.cssSelector("div.a-popover-inner ul li"));
 		SortByButtonPriceLowToHigh.get(1).click();
 		report.endLevel();
-		
-		//Step 11 - Open the button name Sort by, and choose to Sort by: ‚ÄúFeatured‚Äù.	
-		//The Departments chosen is now Sort by: ‚ÄúFeatured‚Äù.
-		report.startLevel("Step 11 - Open the button name Sort by, and choose to Sort by: ‚ÄúFeatured‚Äù.");
+
+		//Step 11 - Open the button name Sort by, and choose to Sort by: “Featured”.	
+		//The Departments chosen is now Sort by: “Featured”.
+		report.startLevel("Step 11 - Open the button name Sort by, and choose to Sort by: “Featured”.");
 		driver.findElements(By.cssSelector("span.a-button-inner")).get(0).click();
 		List<WebElement> SortByButtonFeatured = driver.findElements(By.cssSelector("div.a-popover-inner ul li"));
 		SortByButtonFeatured.get(0).click();
 		report.endLevel();
-		
+
 		// Step 12 - After finished to generate Random Search Term, 'No results for iPhone', in Results Sidebar On Amazon.
 		report.startLevel("Step 12 - After finished to generate Random Search Term, 'No results for iPhone.', in Results Sidebar On Amazon.");
 		WebElement warningMessageTitle2 = driver.findElement(By.cssSelector("div[class='a-row']"));
@@ -105,7 +105,7 @@ public class ResultsSidebarOnAmazon extends AbstractTest{
 		report.startLevel("Step 12.1 - Verify Finished to generate Random Search Term, '" + warningMessageTitleText2 + "', in Results Sidebar On Amazon.");
 		Assert.assertEquals(warningMessageTitleText2, "No results for iPhone.", "Step 12.2 - After finished to generate Random Search Term, 'No results for iPhone.', in Results Sidebar On Amazon."); 
 		report.endLevel();
-		
+
 	}
 	private String generateRandomSearchTerm() {
 		return "iPhone";
